9a2af94588d5a9d826f111b81252e06d936f71f9
Emre-Kahveci <aominemre@gmail.com>
2023-07-22 15:45:58 +0300

    comment line

--- cs-go-glow.py
+++ cs-go-glow.py
@@ -1,7 +1,7 @@
 import pymem
 from time import sleep
 
-# Offsets (converted to decimal notation)
+# Offsets
 localPlayer = 0x00DEA98C
 entityList = 0x04DFFF7C
 glowObjectManager = 0x0535AA08
@@ -11,7 +11,7 @@ glowIndex = 0x010488
 def glow() -> None:
     pm = pymem.Pymem('csgo.exe') # find csgo.exe
     
-    client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
+    client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll # access client.dll
 
     while True:
         local_player = pm.read_int(client + localPlayer)
@@ -20,22 +20,25 @@ def glow() -> None:
         for i in range(64):
             entity = pm.read_int(client + entityList + i * 0x10)
 
+            #if the entity is not empty and not local_player
             if entity == 0 or entity == local_player:
                 continue
             
             entityTeamNum = pm.read_int(entity + teamNum)
             localPlayerTeamNum = pm.read_int(local_player + teamNum)
 
+            #if entity and local_player are not on the same team
             if entityTeamNum == localPlayerTeamNum:
                 continue
             
             glow_Index = pm.read_int(entity + glowIndex)
             glow_Object = glow_Object_Manager + (glow_Index * 0x38)
 
-            pm.write_float(glow_Object + 0x8, 0.0)
-            pm.write_float(glow_Object + 0xC, 1.0)
-            pm.write_float(glow_Object + 0x10, 0.0)
-            pm.write_float(glow_Object + 0x14, 1.0)
+            #glow
+            pm.write_float(glow_Object + 0x8, 0.0) # red
+            pm.write_float(glow_Object + 0xC, 1.0) # green
+            pm.write_float(glow_Object + 0x10, 0.0) # blue
+            pm.write_float(glow_Object + 0x14, 1.0) # alpha
 
             pm.write_bool(glow_Object + 0x27, True)
             pm.write_bool(glow_Object + 0x28, True)
