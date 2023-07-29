db1070d0b7001f49fb0ede75a3ada3fee4e6e75b
Emre-Kahveci <aominemre@gmail.com>
2023-07-29 18:31:42 +0300

    pulling offsets at runtime

--- cs-go-glow.py
+++ cs-go-glow.py
@@ -1,37 +1,39 @@
+import requests
 import pymem
 from time import sleep
 
-# Offsets
-offsets = {
-    'entityList': 0x4DFFF7C,
-    'localPlayer': 0xDEA98C,
-    'glowObjectManager': 0x535AA08,
-    'teamNum': 0xF4,
-    "glowIndex": 0x10488,
-}
+def get_offset(url = "https://raw.githubusercontent.com/frk1/hazedumper/master/csgo.json"):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Check if the request was successful
+        json_data = response.json()
+        return json_data
+    except requests.exceptions.RequestException as e:
+        print(f"Error occurred while fetching the JSON file: {e}")
+        return None
 
-def glow() -> None:
+def glow(offsets = get_offset()) -> None:
     pm = pymem.Pymem('csgo.exe') # find csgo.exe
     
     client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll # access client.dll
     
     while True:
 
-        localPlayer = pm.read_uint(client + offsets["localPlayer"])
-        glowObjectManager = pm.read_uint(client + offsets["glowObjectManager"])
+        localPlayer = pm.read_uint(client + offsets["signatures"]["dwLocalPlayer"])
+        glowObjectManager = pm.read_uint(client + offsets["signatures"]["dwGlowObjectManager"])
 
         for i in range(1,64):
-            entity = pm.read_uint(client + offsets["entityList"] + i * 0x10)
+            entity = pm.read_uint(client + offsets["signatures"]["dwEntityList"] + i * 0x10)
 
             #if the entity is not empty and not localPlayer
             if entity == 0 or entity == localPlayer:
                 continue
             
             #if entity and localPlayer are not on the same team
-            if pm.read_uint(entity + offsets["teamNum"]) == pm.read_uint(localPlayer + offsets["teamNum"]):
+            if pm.read_uint(entity + offsets["netvars"]["m_iTeamNum"]) == pm.read_uint(localPlayer + offsets["netvars"]["m_iTeamNum"]):
                 continue
             
-            glowIndex = pm.read_uint(entity + offsets["glowIndex"])
+            glowIndex = pm.read_uint(entity + offsets["netvars"]["m_iGlowIndex"])
             glowObject = glowObjectManager + (glowIndex * 0x38)
 
             #glow
